91dc6edbbcae04bbbe568fbff50c7cf182e08fc1
Jan Temmerman <jantemme@student.arteveldehs.be>
2019-07-11 01:30:04 +0200

    custom fonts added & general redesign

--- App.js
+++ App.js
@@ -1,16 +1,35 @@
-import React from 'react';
-import Main from './app/Main.js';
+import React from 'react'
+import Main from './app/Main.js'
+import * as Font from 'expo-font'
 
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 export default class App extends React.Component {
+  state = {
+    fontLoaded: false,
+  };
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'open-sans-bold': require('./app/assets/fonts/OpenSans/OpenSans-Bold.ttf'),
+      'open-sans-regular': require('./app/assets/fonts/OpenSans/OpenSans-Regular.ttf')
+    });
+
+    this.setState({ fontLoaded: true });
+  }
+
   render() {
     return (
-      <Main/>
+      this.state.fontLoaded ? (
+        <Main/>
+      ) : null
     );
   }
 }
 
+
 EStyleSheet.build({ // always call EStyleSheet.build() even if you don't use global variables!
-  $textColor: '#0275d8'
-});
+  $textColor: '#0275d8',
+  $openSansBold: 'open-sans-bold',
+  $openSansRegular: 'open-sans-regular',
+})
